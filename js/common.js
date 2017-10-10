087acb4afe2fab8b975ab2d39021151170275893
Mickel Jilenko <mickeljilenko@gmail.com>
2017-10-10 22:33:10 +0400

    Add files

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('form').submit(function(){
+    $("#top_form").submit(function(){
         $.ajax({
             type: 'POST',
             url: 'mail.php',
--- mail.php
+++ mail.php
@@ -10,3 +10,4 @@ $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
